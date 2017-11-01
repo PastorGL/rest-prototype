7fb39ad4cb335c5cef2f56cd74abab891f7c25e9
Alexey Evdokimov <a.evdokimov@russianit.ru>
2017-11-01 17:45:17 +0400

    enum validator

--- resttest/src/main/java/com/locomizer/resttest/contraints/EnumValidator.java
+++ resttest/src/main/java/com/locomizer/resttest/contraints/EnumValidator.java
@@ -5,47 +5,49 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import javax.validation.Payload;
 import java.lang.annotation.*;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Documented
 @Constraint(validatedBy = {EnumValidator.EnumValueValidator.class})
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EnumValidator {
-    public abstract String message() default "Invalid enum value";
+    String message() default "Invalid enum value";
 
-    public abstract Class<?>[] groups() default {};
+    Class<?>[] groups() default {};
 
-    public abstract Class<? extends Payload>[] payload() default {};
+    Class<? extends Payload>[] payload() default {};
 
-    public abstract Class<? extends java.lang.Enum<?>> enumClass();
+    Class<? extends java.lang.Enum<?>> enumClass();
 
-    public abstract boolean ignoreCase() default false;
+    boolean ignoreCase() default false;
 
-    public class EnumValueValidator implements ConstraintValidator<EnumValidator, String> {
+    class EnumValueValidator implements ConstraintValidator<EnumValidator, String> {
         private EnumValidator annotation;
+        private Set<String> enumValues;
 
         @Override
         public void initialize(EnumValidator annotation) {
             this.annotation = annotation;
+            this.enumValues = Arrays.stream(this.annotation.enumClass().getEnumConstants())
+                    .map(Enum::name)
+                    .map(e -> annotation.ignoreCase() ? e.toLowerCase() : e)
+                    .collect(Collectors.toSet());
         }
 
         @Override
         public boolean isValid(String valueForValidation, ConstraintValidatorContext constraintValidatorContext) {
-            boolean result = false;
-
-            Object[] enumValues = this.annotation.enumClass().getEnumConstants();
-
-            if (enumValues != null) {
-                for (Object enumValue : enumValues) {
-                    if (valueForValidation.equals(enumValue.toString())
-                            || (this.annotation.ignoreCase() && valueForValidation.equalsIgnoreCase(enumValue.toString()))) {
-                        result = true;
-                        break;
-                    }
-                }
+            if (valueForValidation == null || valueForValidation.isEmpty()) {
+                return false;
             }
 
-            return result;
+            if (this.annotation.ignoreCase()) {
+                valueForValidation = valueForValidation.toLowerCase();
+            }
+
+            return enumValues.contains(valueForValidation);
         }
     }
 }
